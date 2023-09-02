9daebf45451db2d45b2bd79226dc0cd36cb48d59
mhmdalsaif1999 <120329091+mhmdalsaif1999@users.noreply.github.com>
2023-09-02 23:10:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -722,7 +722,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>🎥 Requested Movie Name : {search} </b>\n\n\n<b>⚠️ Note : If the move you are looking for is not available. Then leave a message admin id below </b> \n\n<b>👤 Contact Admin : @TeamMas_Admin_Bot</b>"
+        cap = f"<b>🎥 Requested Movie Name : {search} </b>\n\n\n<b>⚠️ Note : If the movie you are looking for is not available. Then leave a message admin id below </b> \n\n<b>👤 Admin : @TeamMas_Admin_Bot</b>"
     if imdb and imdb.get('poster'):
         try:
             hehe =  await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
