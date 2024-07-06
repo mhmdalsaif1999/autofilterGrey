9355425e75b858eb149ee113615712c11bffab4e
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2024-07-06 21:02:49 +0530

    Add web support

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from aiohttp import web
+from plugins import web_server
 
 class Bot(Client):
 
@@ -42,6 +44,11 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        port = 8080
+        await web.TCPSite(app, bind_address, port).start()
 
     async def stop(self, *args):
         await super().stop()
